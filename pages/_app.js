052f0cf40493071f9c5acb129b3c1d325a79d9ca
Juan Diego Luna <juandluna17@gmail.com>
2021-08-27 19:38:05 -0500

    Instalando Bootstrap y bootswatch

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/lux/bootstrap.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component { ...pageProps } />
 }
 
 export default MyApp
